c5dcfe49e5f64a38eb97f32575175d87e1082faa
Preston Elliott <preston.elliott24@gmail.com>
2019-06-17 16:45:42 -0400

    updated seed data

--- liverpool-backend/app/serializers/user_serializer.rb
+++ liverpool-backend/app/serializers/user_serializer.rb
@@ -4,10 +4,3 @@ class UserSerializer < ActiveModel::Serializer
   has_many :likes
   attributes :id, :name
 end
-
-
-# EXAMPLE
-# class PostSerializer < ActiveModel::Serializer
-#   has_many :comments
-#   attributes :title, :comments
-# end
