8d5fd200e18c3b8ff44ccbf1cdf5844746d997a0
github username <github email address>
2019-06-17 14:59:36 -0400

    changing to postgresql

--- liverpool-backend/db/schema.rb
+++ liverpool-backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_17_150056) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.integer "user_id"
