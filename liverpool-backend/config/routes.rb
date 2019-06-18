12bc18d3c12d088124089599ab9f56eda43e47ec
Preston Elliott <preston.elliott24@gmail.com>
2019-06-18 10:28:52 -0400

    added index and show to controller

--- liverpool-backend/config/routes.rb
+++ liverpool-backend/config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   get 'comments/index'
   get 'users/index'
 
-  root 'users#home', as: 'home'
   resources :posts
   resources :comments
   resources :users
-  resources :posts, only: [:index, :show]
+  # resources :posts, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
