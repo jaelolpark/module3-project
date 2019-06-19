2354305735bc6917c687669bbb0c97032490590a
github username <github email address>
2019-06-19 10:20:32 -0400

    find pre-existing users

--- liverpool-backend/app/controllers/users_controller.rb
+++ liverpool-backend/app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user   = User.find_by(user_params)
+    @user ||= User.new(user_params)
     if @user.save
       render json: @user, status: :created
     else  
--- liverpool-frontend/index.js
+++ liverpool-frontend/index.js
@@ -3,7 +3,7 @@ const usersUrl = "http://localhost:3000/users"
 const postsUrl = "http://localhost:3000/posts"
 const commentsUrl = "http://localhost:3000/comments"
 const likesUrl = "http://localhost:3000/likes"
-let user_id = 0
+let user_id = null
 let post_id = 0
 
 
@@ -11,111 +11,96 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM content has loaded")
   fetchPosts()
   fetchUsers()
+  const postForm = document.getElementById("post_form")
+  postForm.addEventListener("submit", createPost)
 
-
-  document.addEventListener('click', handleClickEvents)
-
-  function handleClickEvents(e) {
-		e.preventDefault()
-    // console.log(e.target)
-		if(e.target.id === 'userSubmit') createUser(e.target)
-		else if(e.target.id === 'post_submit_button') createPost(e.target)
-    else if(e.target.id === 'btn btn-primary') add_quote(e.target.parentElement)
-	}
-
-
-
-
-
-  // const postForm = document.getElementById("post_form")
-  // postForm.addEventListener("submit", createPost)
-
-  // const userForm = document.getElementById("userForm")
-  // userForm.addEventListener("submit", createUser)
-
+  const userForm = document.getElementById("userForm")
+  userForm.addEventListener("submit", createUser)
+})
 
 function fetchUsers() {
-  fetch(usersUrl)
+  fetch (usersUrl)
   .then(resp => resp.json())
-  .then(json => {json.forEach(displayUser)})
+  .then(data => data.forEach(user => {displayUser(user)
+  }))
 }
 
 function fetchPosts() {
-  fetch(postsUrl)
+  fetch (postsUrl)
   .then(resp => resp.json())
-  .then(json => {json.forEach(displayPost)})
+  .then(data => data.forEach(post => {displayPost(post)
+  }))
 }
-
-function fetchLikes() {
-  fetch(likesUrl)
-  .then(resp => resp.json())
-  .then(json => {json.forEach(displaylike)})
-}
-
-function fetchComments() {
-  fetch(commentsUrl)
-  .then(resp => resp.json())
-  .then(json => {json.forEach(displaycomment)})
-}
-
-
 
 // WORKING
 function createUser(e) {
-  // console.log(userName.name)
-  let user = {name: userName.value}
+  e.preventDefault()
+  let user = {name: e.target.name.value}
   fetch(usersUrl, {
     method: "POST",
     headers: {
       "Accept": "application/json",
       "Content-Type": "application/json"
-     }, 
+  }, 
     body: JSON.stringify(user)
+  }).then(res => res.json())
+  .then(e.target.reset())
+  .then(res => {
+    console.log("This is my user: ", res)
+    user_id = res.id
+    console.log("This is the user_id: ", user_id)
+    displayUser(res)
   })
-  .then(res => res.json())
-  .then(res => displayUser(res))
-  userForm.reset()
 }
-// NOT WORKING BUT NEED LOGIN FIRST
-function createPost(e) {
-  let postInput = {
-    title: title_input.value,
-    content: content_input.value,
-    media: media_input.value,
-    user_id: user_id
-  }
-  fetch(postsUrl, {
+
+// WORKING
+function createUser(e) {
+  e.preventDefault()
+  let user = {name: e.target.name.value}
+  fetch(usersUrl, {
     method: "POST",
     headers: {
       "Accept": "application/json",
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(postInput)
+      "Content-Type": "application/json"
+  }, 
+    body: JSON.stringify(user)
+  }).then(res => res.json())
+  .then(e.target.reset())
+  .then(res => {
+    console.log("This is my user: ", res)
+    user_id = res.id
+    console.log("This is the user_id: ", user_id)
+    displayUser(res)
   })
-  .then(res => res.json())
-  .then(displayPost)
-   post_form.reset()
 }
 
-// SETUP COMMENTS IN HTML
-// function createComment(e) {
-//   e.preventDefault()
-//   let commentInput = {
-//     text: e.target.text.value,
-//     user_id: user_id,
-//     post_id: post_id
-//   }
-//   fetch(commentsUrl, {
-//     method: "POST",
-//     headers: {
-//       "Accept": "application/json",
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(commentInput)
-//   }).then(res => res.json())
-//   .then(res => {(displayComment(res))
-//   })
-// }
+// WORKING BUT NEED LOGIN FIRST
+function createPost(e) {
+  e.preventDefault()
+  
+  if(user_id === null) {
+    alert('You have to be logged in to post!')
+  }
+  else {
+    let postInput = {
+      title: e.target.title.value,
+      content: e.target.content.value,
+      media: e.target.media.value,
+      user_id: user_id
+    }
+    fetch(postsUrl, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postInput)
+    }).then(res => res.json())
+    .then(console.log(postInput))
+    .then(displayPost(postInput))
+  }
+  document.getElementById("post_form").reset()
+}
 
 function displayPost(post) {
   const postList = document.getElementById("post-list")
@@ -124,7 +109,6 @@ function displayPost(post) {
       <h2>${post.title}</h2>
       <p>${post.content}</p>
     </div>`
-  // console.log(post)
 }
 // Display Users in a modal when clicked on
 function displayUser(user){
@@ -132,10 +116,3 @@ function displayUser(user){
   modalBody.innerHTML +=
     `<li>${user.name}</li>`
 }
-
-
-
-
-
-
-})
