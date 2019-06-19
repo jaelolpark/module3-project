4f3b3345559f9fe469543f8b7e70c8c8631b2732
github username <github email address>
2019-06-19 01:05:17 -0400

    refactor some, validate, fetch

--- liverpool-backend/app/models/post.rb
+++ liverpool-backend/app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :likes
     has_many :comments
+
+    validates :title, {
+        presence: true,
+    }
+    validates :content, {
+        presence: true,
+    }
 end
